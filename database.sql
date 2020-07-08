f7900aaaf99bc3c35399adec84b783be14bcc2a8
Ailea Patrinely <aileapatrinely@gmail.com>
2020-07-08 14:51:59 -0500

    Sets up server.js and pool.js

---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "books" (
+"id" SERIAL PRIMARY KEY,
+"title" VARCHAR(250),
+"author" VARCHAR(100),
+"published" DATE);
---
+++ modules/pool.js
@@ -0,0 +1,19 @@
+const pg = require('pg');
+
+const Pool = pg.Pool;
+
+const pool = newPool({
+  database: 'bookstore',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 3000,
+});
+
+pool.on('connect', () => {
+  console.log('Pool Connected');
+});
+
+pool.on('error', (error) => {
+  console.log('NOPE! Pool not connected');
+});
